035911a908a0da017e4ff234c66f1f900d6461f3
lindaline <68811165+lindaline@users.noreply.github.com>
2026-04-13 23:57:37 +0800

    SideBar&DashBoard

---
+++ components/LayoutBody.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Siderbar from '@/app/sidebar/page';
+
+const noSidebarPages = ['/', '/login'];
+
+const login_state = true;
+
+export default function LayoutBody({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideSidebar = noSidebarPages.includes(pathname);
+
+  return (
+    <body>
+      {!login_state ? (
+        <>{children}</>
+      ) : (
+        <>
+          <Siderbar />
+          <main className="main-content">{children}</main>
+        </>
+      )}
+    </body>
+  );
+}
